0286dc84102589f68fcb17fa56b32bb973bd484b
Vitor0389 <vitor0389@gmail.com>
2025-05-01 14:22:39 -0300

    iniciando classe campeonato
    feat : adionado lista de partidas na classe Campeonato
    feat : adicionado lista de fases na classe Campeonato
    feat : adicionado método de criação de campeonato

--- src/main/java/br/ifsp/demo/model/Campeonato.java
+++ src/main/java/br/ifsp/demo/model/Campeonato.java
@@ -1,5 +1,13 @@
 package br.ifsp.demo.model;
 
 public class Campeonato {
-    
+
+    private List<Partida> partidasList;
+
+    private List<Fase> fasesList;
+
+
+    public createCampeonato(List<Time> times){
+
+    }
 }
