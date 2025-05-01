a32a54b31932b2c1f07bcf5acd55894967c841e0
Vitor0389 <vitor0389@gmail.com>
2025-05-01 13:57:38 -0300

    criando o primeiro teste
    test(C01/US01) : adicionado o primeiro teste que checa se há sucesso com 32 times cadastrados

--- src/test/java/br/ifsp/demo/DemoAuthAppApplicationTests.java
+++ src/test/java/br/ifsp/demo/DemoAuthAppApplicationTests.java
@@ -1,13 +1,22 @@
 package br.ifsp.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 class DemoAuthAppApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    @DisplayName("Testando se há sucesso na criação do campeonato com 32 times.")
+    void testingSuccessWith32Teams(){
+
+        assertThat(CampeonatoService.createCampeonato()).isEqualto(true);
+
     }
 
 }
