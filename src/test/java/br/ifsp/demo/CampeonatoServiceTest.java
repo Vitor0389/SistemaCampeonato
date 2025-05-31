a645628f521903e869740e76d37273db3b1f3f70
caio-soares13 <caio.vitsoares@gmail.com>
2025-05-31 20:07:50 -0300

    test: adiciona teste de mutação que mata mutante no setChave nas primeiras partidas do campeonato

--- src/test/java/br/ifsp/demo/CampeonatoServiceTest.java
+++ src/test/java/br/ifsp/demo/CampeonatoServiceTest.java
@@ -714,5 +714,20 @@ public class CampeonatoServiceTest {
     }
 
 
+    @Tag("Unit Test")
+    @Tag("Mutation")
+    @ParameterizedTest
+    @DisplayName("Deve atribuir corretamente o número da chave às partidas")
+    @MethodSource("provide4Teams")
+    void deveAtribuirChavesCorretamenteNasPartidas(List<Team> teams) {
+
+        Campeonato campeonato = Campeonato.createCampeonato("Teste", teams);
+        List<Partida> partidas = campeonato.getCurrentFase().getPartidas();
+
+        assertThat(partidas.get(0).getChave()).isEqualTo(0);
+        assertThat(partidas.get(1).getChave()).isEqualTo(1);
+    }
+
+
 
 }
