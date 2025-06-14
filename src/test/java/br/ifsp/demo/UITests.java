7da57d5227e889fb1cd708820c502e45e6a73299
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-14 17:43:55 -0300

    test: should register account and login test

--- src/test/java/br/ifsp/demo/UITests.java
+++ src/test/java/br/ifsp/demo/UITests.java
@@ -50,5 +50,37 @@ public class UITests extends BaseSeleniumTest{
         delay(1000);
     }
 
+    @Test
+    @DisplayName("Should register account and login")
+    @Tag("UiTest")
+    public void shouldRegisterAccountAndLogin() {
+        driver.get("http://localhost:3000/login");
+        final WebElement registerButton =
+                driver.findElement(By.xpath("//button[not(@type='submit')]"));
+        delay(500);
+        registerButton.click();
+        delay(500);
 
+        final WebElement inputName =
+                driver.findElement(By.xpath("//input[@placeholder='Nome']"));
+        final WebElement inputSurname =
+                driver.findElement(By.xpath("//input[@placeholder='Sobrenome']"));
+        final WebElement inputEmail =
+                driver.findElement(By.xpath("//input[@placeholder='E-mail']"));
+        final WebElement inputPassword =
+                driver.findElement(By.xpath("//input[@placeholder='Senha']"));
+
+        inputName.sendKeys("Pedro");
+        inputSurname.sendKeys("Candido");
+        inputEmail.sendKeys("pedro@email.com");
+        inputPassword.sendKeys("123456");
+        delay(1000);
+        registerButton.click();
+        delay(1000);
+
+        final WebElement loginButton =
+                driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+        delay(2000);
+    }
 }
