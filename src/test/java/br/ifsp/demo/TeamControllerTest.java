1c0dd6d904fca6f8723b74eea15f36a82bc9a7e3
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-15 15:51:00 -0300

    test: should get all teams

--- src/test/java/br/ifsp/demo/TeamControllerTest.java
+++ src/test/java/br/ifsp/demo/TeamControllerTest.java
@@ -3,11 +3,18 @@ package br.ifsp.demo;
 import br.ifsp.demo.controller.TeamController;
 import br.ifsp.demo.model.Team;
 import br.ifsp.demo.repository.TeamRepository;
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 public class TeamControllerTest {
@@ -20,5 +27,13 @@ public class TeamControllerTest {
         Team team1 = new Team(UUID.randomUUID(), "Cortinthians");
         Team team2 = new Team(UUID.randomUUID(), "Flamengo");
         Team team3 = new Team(UUID.randomUUID(), "São Paulo");
+    }
+
+
+    @Test
+    @DisplayName("Should get all teams")
+    public void getAllTeams() {
+        List<Team> teams = repository.findAll();
+        assertThat(teams).hasSize(34);
     }
 }
