4e6de5e420108d840e149d54bd2166da3d41fa8e
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-15 16:33:56 -0300

    refactor: implementing mockMvc for should return all saved teams test, improving GET requisition test

--- src/test/java/br/ifsp/demo/TeamControllerTest.java
+++ src/test/java/br/ifsp/demo/TeamControllerTest.java
@@ -1,39 +1,46 @@
 package br.ifsp.demo;
 
 import br.ifsp.demo.controller.TeamController;
-import br.ifsp.demo.model.Team;
 import br.ifsp.demo.repository.TeamRepository;
-import org.assertj.core.api.ListAssert;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 
-import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 public class TeamControllerTest {
 
+    @Autowired MockMvc mockMvc;
     @Autowired TeamRepository repository;
     @Autowired TeamController controller;
 
-    @BeforeEach
-    public void setUp() {
-        Team team1 = new Team(UUID.randomUUID(), "Cortinthians");
-        Team team2 = new Team(UUID.randomUUID(), "Flamengo");
-        Team team3 = new Team(UUID.randomUUID(), "São Paulo");
-    }
-
-
     @Test
-    @DisplayName("Should get all teams")
-    public void getAllTeams() {
-        List<Team> teams = repository.findAll();
-        assertThat(teams).hasSize(34);
+    @DisplayName("Should return all saved teams")
+    void shouldReturnAllSavedTeams() throws Exception {
+        mockMvc.perform(get("/api/v1/teams") // Faz a requisição GET
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(35)));
     }
+
+
 }
