828ed96c0f1e3567a5b8d7875a5110ba69d3a0b0
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-16 09:59:32 -0300

    test: should create a new team test

--- src/test/java/br/ifsp/demo/TeamControllerTest.java
+++ src/test/java/br/ifsp/demo/TeamControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -51,5 +52,17 @@ public class TeamControllerTest extends BaseApiIntegrationTest{
                 .containsExactlyInAnyOrderElementsOf(databaseIds);
     }
 
+    @Test
+    @DisplayName("Should create a new team")
+    @Tag("IntegrationTest")
+    void shouldCreateNewTeam() {
+        TeamDTO newTeamDTO = new TeamDTO(UUID.randomUUID(), "Paysandu");
+        List<TeamDTO> teamDTOs = List.of(newTeamDTO);
 
+        given().contentType("application/json").port(port).body(teamDTOs)
+                .when().post("/api/v1/teams")
+                .then().statusCode(201)
+                .contentType(ContentType.JSON)
+                .extract().body().jsonPath().getList(".", TeamDTO.class);
+    }
 }
