ce9ac614392b08435c2656777c00108d913ba7b9
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-22 03:21:05 -0300

    feat: creating CreateChampionshipPageObject for create championship page UI tests

---
+++ src/test/java/br/ifsp/demo/CreateChampionshipPageObject.java
@@ -0,0 +1,49 @@
+package br.ifsp.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class CreateChampionshipPageObject extends BasePageObject {
+    private static final String PAGE_URL = "http://localhost:3000/criar-campeonato";
+
+    @FindBy(xpath = "//h2[text()='Criar Campeonato']")
+    private WebElement pageTitle;
+
+    @FindBy(xpath = "//div[label[text()='Nome do Campeonato:']]//input[@type='text']")
+    private WebElement inputChampionshipName;
+
+    @FindBy(xpath = "//button[@type='submit' and text()='Criar']")
+    private WebElement createBtn;
+
+    public CreateChampionshipPageObject(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean verifyActualPage() {
+        return super.verifyURL(PAGE_URL);
+    }
+
+    public void writeChampionshipName(String nome) {
+        inputChampionshipName.sendKeys(nome);
+    }
+
+    public void selectTeam(String nomeDoTime) {
+        String xpathTime = String.format("//label[contains(., '%s')]/input[@type='checkbox']", nomeDoTime);
+        WebElement checkboxTime = driver.findElement(By.xpath(xpathTime));
+        checkboxTime.click();
+    }
+
+    public void selectTeams(List<String> nomesDosTimes) {
+        for (String nome : nomesDosTimes) {
+            selectTeam(nome);
+        }
+    }
+
+    public void clickCreateBtn() {
+        createBtn.click();
+    }
+}
