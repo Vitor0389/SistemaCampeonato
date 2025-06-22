3f7e698dbe5c5f030d937954f31a4c177df2a63d
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-22 03:18:56 -0300

    feat: creating HomePageObject for main page ui tests

---
+++ src/test/java/br/ifsp/demo/HomePageObject.java
@@ -0,0 +1,36 @@
+package br.ifsp.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePageObject extends BasePageObject {
+    private static final String PAGE_URL = "http://localhost:3000/main";
+
+    @FindBy(xpath = "//h2[text()='Menu Principal']")
+    private WebElement mainTitle;
+
+    @FindBy(xpath = "//button[text()='Criar Campeonato']")
+    private WebElement createChampionshipBtn;
+
+    @FindBy(xpath = "//button[contains(@class, 'logout') and text()='Sair']")
+    private WebElement exitBtn;
+
+    public HomePageObject(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean verifyActualPage() {
+        return verifyURL(PAGE_URL);
+    }
+    
+    public CreateChampionshipPageObject clickCreateChampionshipBtn() {
+        createChampionshipBtn.click();
+        return new CreateChampionshipPageObject(driver);
+    }
+
+    public LoginPageObject clickExitBtn() {
+        exitBtn.click();
+        return new LoginPageObject(driver);
+    }
+}
